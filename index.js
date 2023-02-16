d211f8ce3bc73e380a88dc3b2fe9ef10ce453a3e
Smlvrc <suleymanovasema50@gmail.com>
2023-02-16 05:03:05 -0800

    first commit

---
+++ index.js
@@ -0,0 +1,120 @@
+// birinci tapsiriq
+// const array=[23,45,22,36,-45,84];
+// const ikinci=[1,2,3,4];
+
+// function sum(bir){
+// var cem=0;    
+// for(i=0;i<bir.length;i++){
+// cem=cem+bir[i];
+//  }
+//  return cem;
+// }
+// console.log(sum(array))
+// console.log(sum(ikinci))
+
+// ikinci tapsiriq
+
+// const array=[23,45,22,36,-45,"sema",11,100];
+// function sum(bir){
+// var cem=0;    
+// for(i=0;i<bir.length;i++){
+//     if(typeof(bir[i])=="number"){
+//         cem=cem+bir[i];}
+//   }
+//  return cem;
+// }
+
+// console.log(sum(array))
+
+// ucuncu
+// const array=[1, 5, 20, 32, 54, 79, 82, 91, 564, 351]
+// function hesap(bir){
+//     var cem=0;
+//     var tek=0;
+//     for(i=0;i<bir.length; i++){
+//        if(bir[i]%2==0){
+//      cem++;
+//        }
+//     else
+//     tek++;     
+//     }
+//     console.log("cem:",cem,"tek:",tek);
+//     return ;
+// }
+// hesap(array);
+
+// dorduncu
+// const array=['Tofiq', 'Ruslan', 'Səma', 3, null, 'Məhəmməd', {'type': "user"}, [1, 2, 3]]
+// function string(b){
+//     for( i=0;i<b.length;i++){
+//         if(typeof(b[i])==="string"){
+//             console.log(b[i]);
+//         }  }
+//     return;
+// }
+// string(array);
+
+// besinci
+// var library = [
+//     {
+//         author: 'Bill Gates',
+//         title: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Steve Jobs',
+//         title: 'Walter Isaacson',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Suzanne Collins',
+//         title: 'Mockingjay: The Final Book of The Hunger Games',
+//         readingStatus: false
+//     }
+//  ];
+
+//  function a(){
+//     for(i=0;i<library.length;i++)
+//     console.log((library[i].title))
+//  }
+//  a()
+
+// 6-ci
+// var library = [
+//     {
+//         author: 'Bill Gates',
+//         title: 'The Road Ahead',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Steve Jobs',
+//         title: 'Walter Isaacson',
+//         readingStatus: true
+//     },
+//     {
+//         author: 'Suzanne Collins',
+//         title: 'Mockingjay: The Final Book of The Hunger Games',
+//         readingStatus: false
+//     }
+//  ]
+//   function a(){
+//     var cem=0;
+//     for(i=0;i<library.length;i++){
+//     if(library[i].readingStatus==true)
+//    console.log("true")
+//  }}
+//  a()
+
+
+// 7ci
+// const array =[1, 1, 5, 'Mahammad', 'Mahammad', null, null, null, undefined, undefined, 4, 5, 5, 4]
+// var b;
+// var cem=0;
+// for(i=0;i<array.length;i++){
+
+// if(b==array[i]){
+// cem++;
+// }
+//     b=array[i]
+// }
+// console.log(cem)
